00d52f0ae7628d76ab235e431c0810f49ee9dbc2
Aurora0043 <940403army@gmail.com>
2024-10-14 15:39:56 +0800

    v3.0

--- app/src/main/java/com/example/composebasic/MainActivity.kt
+++ app/src/main/java/com/example/composebasic/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +13,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,13 +40,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hi $name",
-        color = Color.Blue,
-        fontSize = 30.sp,
-        fontFamily = FontFamily(Font(R.font.hand)),
-        modifier = modifier,
-    )
+    Column {
+        Text(
+            text = stringResource(R.string.author),
+            color = Color.Blue,
+            fontSize = 30.sp,
+            fontFamily = FontFamily(Font(R.font.hand)),
+            modifier = modifier,
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ccc),
+            contentDescription = "花丸",
+            modifier = modifier,
+        )
+    }
 }
 
 @Preview(showBackground = true)
