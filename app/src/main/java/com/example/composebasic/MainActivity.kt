58cbb72b3f447649c489b7b57fd10181ea6cdfad
Aurora0043 <940403army@gmail.com>
2024-10-14 16:25:03 +0800

    v5.0

--- app/src/main/java/com/example/composebasic/MainActivity.kt
+++ app/src/main/java/com/example/composebasic/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,6 +15,10 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -45,14 +50,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var x by remember { mutableStateOf(0) }
     Column {
+       Text(text =x.toString(),fontSize =50.sp, modifier = modifier)
+
         Row {
             Text(
                 text = stringResource(R.string.author),
                 color = Color.Blue,
                 fontSize = 30.sp,
                 fontFamily = FontFamily(Font(R.font.hand)),
-                modifier = modifier,
+                modifier = Modifier.clickable { x++ },
             )
             Image(
                 painter = painterResource(id = R.drawable.ccc),
